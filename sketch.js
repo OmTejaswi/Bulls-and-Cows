38608fc0ec3b0d0a183e6240243f807163d94fd0
Om Tejaswi <omtejaswi2008classes@gmail.com>
2021-03-07 15:05:32 +0530

    first commit

---
+++ sketch.js
@@ -0,0 +1,76 @@
+var word, bull , cow;
+var rand;
+var s;
+var cword;
+var input, submitBtn;
+
+
+function setup(){
+    word = createElement("h2").html("Your  Word").position(400,25);
+    bull = createElement("h2").html("Bulls").position(600,25);
+    cow = createElement("h2").html("Cows").position(800,25);
+    input = createInput("","[A-Za-z]").attribute("placeholder","Guess A Word").position(550,
+        350);
+
+    submitBtn = createButton("SUBMIT").position(600,400)
+    rand = Math.round(random(0,s.length-1))
+    cword = s[rand];
+}
+
+function draw() {
+    submitBtn.mousePressed(function(){
+        var wo = input.value();
+        var lowerCase = wo.toLowerCase();
+        var bulls = 0, cows = 0; 
+
+        console.log(lowerCase)
+        if(wo.length !== 4) {
+            alert("Please Eneter A Valid 4 Letter Word");
+        }
+
+        if(cword.charAt(0) === lowerCase.charAt(0)){
+            bulls+=1;
+        }
+        if(cword.charAt(1) === lowerCase.charAt(1)){
+            bulls+=1;
+        }
+        if(cword.charAt(2) === lowerCase.charAt(2)){
+            bulls+=1;
+        }
+        if(cword.charAt(3) === lowerCase.charAt(3)){
+            bulls+=1;
+        }
+
+        if(cword.charAt(0) === lowerCase.charAt(1) ||
+            cword.charAt(0) === lowerCase.charAt(2) ||
+            cword.charAt(0) === lowerCase.charAt(3) ||
+            cword.charAt(0) === lowerCase.charAt(0)) {
+                cows += 1;
+            }
+
+            if(cword.charAt(1) === lowerCase.charAt(0) ||
+                cword.charAt(1) === lowerCase.charAt(2) ||
+                cword.charAt(2) === lowerCase.charAt(3) || 
+                cword.charAt(1) === lowerCase.charAt(1)) {
+                    cows += 1;
+            }
+
+            if(cword.charAt(2) === lowerCase.charAt(0) ||
+                cword.charAt(2) === lowerCase.charAt(1) ||
+                cword.charAt(2) === lowerCase.charAt(3) || 
+                cword.charAt(2) === lowerCase.charAt(2)) {
+                    cows += 1;
+            }
+
+            if(cword.charAt(3) === lowerCase.charAt(0) ||
+                cword.charAt(3) === lowerCase.charAt(2) ||
+                cword.charAt(3) === lowerCase.charAt(1) || 
+                cword.charAt(3) === lowerCase.charAt(3)) {
+                    cows += 1;
+            }
+
+        console.log("bulls:"+bulls)
+        console.log("cows:"+cows)
+    })
+
+}
